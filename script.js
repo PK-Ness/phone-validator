91dc1072c0265f66fecd998180739aabc1e7d0ad
Ness <sampaio_88@hotmail.com>
2024-09-01 19:09:52 +0100

    JS codes for the buttons and input

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const input = document.getElementById('input');
+const userInput = document.getElementById('user-input');
+const clearButton = document.getElementById('clear-btn');
+const checkButton = document.getElementById('check-btn');
+clearButton.addEventListener('click', () => {
+    userInput.value = '';
+});
+checkButton.addEventListener('click', () => {
+    if (userInput.value.trim() === '') {
+        alert('Please provide a phone number');
+    }
+});
