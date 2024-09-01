da4096165e69d09970011d5958b9379862c37d17
Ness <sampaio_88@hotmail.com>
2024-09-01 19:17:31 +0100

    Finished JS code

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const userInput = document.getElementById('user-input');
 const clearButton = document.getElementById('clear-btn');
 const checkButton = document.getElementById('check-btn');
+const resultsDiv = document.getElementById('results-div');
 clearButton.addEventListener('click', () => {
     userInput.value = '';
+    resultsDiv.innerHTML = '';
 });
 checkButton.addEventListener('click', () => {
+    const phoneNumber = userInput.value.trim();
+    const usPhoneNumberPattern = /^(1\s*)?\(?\d{3}\)?[\s-]*\d{3}[\s-]*\d{4}$/;
     if (userInput.value.trim() === '') {
         alert('Please provide a phone number');
-    }
+    } else if (usPhoneNumberPattern.test(phoneNumber)) {
+     resultsDiv.innerHTML = `Valid US number: ${phoneNumber}`;
+     resultsDiv.className = '';
+            }else {
+resultsDiv.innerHTML = `Invalid US number: ${phoneNumber}`;
+                resultsDiv.className = 'invalid'; 
+            }
 });
+
